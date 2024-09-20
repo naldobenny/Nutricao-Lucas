bc5d1bb7c7eee35ad3b420ef7a08300f63752415
Lucas <133382705+naldobenny@users.noreply.github.com>
2024-09-20 12:12:45 -0300

    20/09/24

---
+++ main.js
@@ -0,0 +1,12 @@
+alert("Olá Lucas")
+console.log("Oi João");
+
+var título = document.querySelector("h1");
+título.textContent = "Lucas Nutrição";
+
+var trPaulo = document.querySelector("#Paulo");
+
+console.log(trPaulo);
+
+var tdPeso = trPaulo.querySelector(".info-peso");
+tdPeso.textContent = "150";
